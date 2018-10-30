a0e400699866097a1cfd14c8afa4c0f0329cd089
LAPTOP-NV2K93JA\MYPC <youshj1004@itcommerce.co.kr>
2018-10-30 21:22:35 +0900

    입력받은 월의 최대일 구하는 로직 업데이트

--- src/shj/calendar/Calendar.java
+++ src/shj/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package shj.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
 		
@@ -10,5 +12,15 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		
+		int[] endDate = {31,28,31,30,31,30,31,31,30,31,30,31};
+		
+		System.out.print("최대일수를 출력하기 원하는 달을 입력하시오.");
+		Scanner sr = new Scanner(System.in);
+		int month = sr.nextInt();
+		
+		System.out.printf("%d는 %d일까지 있습니다.",month,endDate[month-1]);
+		sr.close();
 	}
 }
