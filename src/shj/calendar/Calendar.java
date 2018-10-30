b6aaa72514d687e0e308d19d8251c66a26c4ad7b
Seong <tjdghkwnd1@naver.com>
2018-10-30 16:45:16 +0900

    요일부분 한글깨짐 현상 체크를 위한 commit

--- src/shj/calendar/Calendar.java
+++ src/shj/calendar/Calendar.java
@@ -3,7 +3,7 @@ package shj.calendar;
 public class Calendar {
 	public static void main(String[] args) {
 		
-		System.out.println(" 일   월    화   수    목    금    토");
+		System.out.println(" 일   월    화   수    목    금    토"); // 한글 깨지는 부분 체크를 위한 주석
 		System.out.println("---------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
